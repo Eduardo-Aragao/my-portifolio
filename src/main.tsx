954b65727143e0069d4aced33323f1a27601017e
Eduardo-Aragao <eduardo.aragaosoft@outlook.com>
2025-09-16 21:36:48 -0400

    fix: correct import path for user service

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import theme from './Theme.ts'
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme.ts';
 
 
 createRoot(document.getElementById('root')!).render(
